6786c0f5755330a4e44e6f9f8e3b1882bda867eb
zhouteng <wwwsousoucom@126.com>
2019-02-05 16:54:59 +0800

    guess number

--- readme.md
+++ readme.md
@@ -1,3 +1,30 @@
 # rust
 
-Strings Tuples Arrays Slices 
+vscode 调试文件
+
+launch.json
+
+```
+{
+    // Use IntelliSense to learn about possible attributes.
+    // Hover to view descriptions of existing attributes.
+    // For more information, visit: https://go.microsoft.com/fwlink/?linkid=830387
+    "version": "0.2.0",
+    "configurations": [
+        {
+            "type": "lldb",
+            "request": "launch",
+            "name": "play_ground",
+            "program": "${workspaceRoot}/target/debug/play_ground",
+            "args": [],
+            "cwd": "${workspaceRoot}/target/debug/"
+        }
+    ]
+}
+```
+
+rust编译命令 
+
+    rustc main.rs
+
+# PROGRAMMING A GUESSING GAME
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,32 @@
+extern crate rand;
+// We add a line that lets Rust know we'll be using the rand crate as external dependency.
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+// The Rng trait defines methods that random number generators implement,
+// and this trait must be in scope for us to use those method
+
 fn main() {
-    let t = (1, 'a', false);
-    let f = (2, (1, 'a', false));
-    print!("{} {} {}", t.0, t.1, t.2);
-    print!("{:#?}", f.1);
+    println!("Guess the number!");
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    println!("The secret number is: {}", secret_number);
+    loop {
+        println!("Please input your guess.");
+        let mut guess= String::new();
+        io::stdin().read_line(&mut guess).expect("Failed to read line");
+        let guess:u32 = match guess.trim().parse(){
+            Ok(num) =>num,
+            Err(_) => continue,
+        };
+        println!("You guessed: {}", guess);
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
+
